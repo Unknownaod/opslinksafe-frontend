240ec81ba80654176c46e2883bcf807fb6f05b47
Unknownaod <unknown@dona.pw>
2025-12-11 17:21:30 -0500

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        opslink: {
+          blue: "#4e8cff",
+          dark: "#050812"
+        }
+      }
+    }
+  },
+  plugins: []
+};
